79aa14398a3ce73d4a074404c78e425cf52e83ab
ibraim martinez <ibraimm5@gmail.com>
2021-11-12 20:44:49 -0500

    adding get route

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -3,7 +3,6 @@ import { useMutation } from '@apollo/react-hooks';
 import { Form, Button, Alert } from 'react-bootstrap';
 import { ADD_USER } from '../utils/mutations';
 
-import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
