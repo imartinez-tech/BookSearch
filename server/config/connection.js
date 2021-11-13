537c165b5b542f257b968872f97d273ce93bf49c
ibraim martinez <ibraimm5@gmail.com>
2021-11-12 21:42:03 -0500

    deploying to heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/bookDB', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/booksDB', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
